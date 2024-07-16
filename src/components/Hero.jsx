658778672d6bd1dca477470943571a4d4fee310f
Atilla0001 <toulouse663831@gmail.com>
2024-07-16 12:21:33 +0300

    navbar and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { ReactTyped } from "react-typed";
+
+
+
+
+const Hero = () => {
+    return (
+        <div className='text-white'>
+            <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold p-2'>GROWİNG WİTH DATA ANALITICS</p>
+                <h1 className='md:text-7xl text-4xl sm:text-3xl font-bold md:py-6'>GROW WİTH ATİ BABA</h1>
+                <div className='flex justify-center'>
+                    <p className='md:text-5xl sm:text-4xl text-xl font-bold items-center'>Fast, flexible financing for</p>
+                    <ReactTyped
+                        className='md:text-5xl sm:text-4xl text-xl text-gray-500 font-bold pl-1'
+                        strings={["Saas","BTC","FOREX"]}
+                        typeSpeed={250}
+                        loop
+                    />
+                </div>
+                <ReactTyped
+                        className='text-[12px] font-bold text-gray-600'
+                        strings={["Here you can find anything you want"]}
+                        typeSpeed={20}
+                        loop
+                    />
+                    <button className='flex justify-center text-white font-bold bg-[#00df9a] max-w-[150px] rounded mx-auto px-8 py-1 mt-1'>Get Started</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { IoMenuOutline } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+
+
+const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
+
+  const clicknav = () => {
+        setNav(!nav)
+    }
+
+
+    return (
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+            <h1 className='w-full text-3xl font-bold text-[#8debbc]'>AtiBaba</h1>
+            <ul className=' hidden md:flex'>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
+            <div onClick={clicknav} className='block md:hidden' >
+                {nav ? <IoMenuOutline size={20} ></IoMenuOutline> : <IoMdClose></IoMdClose>}
+                </div>
+            <div className={!nav? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000600] m-4 ease-in-out duration-500" : "fixed left-[-100%]"}>
+                <h1 className='w-full text-3xl font-bold text-[#8debbc]'>AtiBaba</h1>
+                <ul className=' uppercase'>
+                    <li className='p-4 border-b border-gray-600'>Home</li>
+                    <li className='p-4 border-b border-gray-600'>Company</li>
+                    <li className='p-4 border-b border-gray-600'>Resources</li>
+                    <li className='p-4 border-b border-gray-600'>About</li>
+                    <li className='p-4'>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
